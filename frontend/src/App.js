20123affe74b6266ef630266e46e82320d2fd296
Anis Merchant <anisxmerchant@gmail.com>
2018-11-21 19:50:40 -0500

    project ten: Refactor front-end structure as per Meera's feedback

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TopBar from './TopBar';
+import Sidebar from './Sidebar';
+import Main from './Main';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         {/* TODO: Add Routes and Components */}
+        <TopBar />
+        <Sidebar />
+        <Main />
       </div>
     );
   }
---
+++ frontend/src/TopBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from '.react';
+
+class TopBar extends Component {
+    render() {
+         return (
+            <div></div>
+         )
+    }
+}
+
+export default TopBar;
