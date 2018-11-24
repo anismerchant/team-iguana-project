11b428f8a1e748ffa1caf20767461eab98fd00be
Anis Merchant <anisxmerchant@gmail.com>
2018-11-24 15:00:25 -0500

    project ten: Setup inventory-list html structure with global css resets

--- frontend/src/InventoryList.js
+++ frontend/src/InventoryList.js
@@ -5,9 +5,17 @@ export default class InventoryList extends React.Component {
     render() {
 
         return (
-
             <div className="inventoryList">
-                This is the Inventory List view!
+               <div className="inventoryList__container">
+                    <div className="inventoryList__heading">
+                        <div className="inventoryList__form--product-heading"> 
+                        
+                        </div>
+                        <div className="InventoryList__form--product-details">
+
+                        </div>
+                    </div>
+               </div>
             </div>
         );
     }
