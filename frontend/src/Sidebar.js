f4cf76023b6809bf6c0a9b4555a5c4efb77ee5ba
gizraelian <g.izraelian@gmail.com>
2018-11-25 15:05:08 -0500

    temporary merge conflict troubleshooting commit

--- frontend/src/Sidebar.js
+++ frontend/src/Sidebar.js
@@ -3,20 +3,24 @@ import React, { Component } from 'react';
 class Sidebar extends Component {
     render() {
          return (
-            <div className="sidebar">
-                <div className="sidebar__topContainer">
-                
-                    <div>INSTOCK</div>
-                </div>
-                <div className="sidebar__links">
-                    <div className="sidebar__links--Inventory">Inventory</div>
-                    <div className="sidebar__links--Locations">Locations</div>
-                    <div className="sidebar__links--Users">Users</div>
-                </div>
-                <div className="sidebar__bottomVersionText">Version 1.0</div>
-            </div>
+            <form className="sidebar" id="sidebarForm">
+                    <img src="./Assets/wordmark/wordmark.svg" className="sidebar__topLogo"/>
+                    <div className="sidebar__links">
+                        <button className="sidebar__links--link">
+                            <img src="./Assets/Icons/Inventory.svg" className="sidebar__links--img"/>
+                        Inventory</button>
+                        <button className="sidebar__links--link">
+                            <img src="./Assets/Icons/Location.svg" className="sidebar__links--img"/>
+                        Locations</button>
+                        <button className="sidebar__links--link">
+                            <img src="./Assets/Icons/User.svg" className="sidebar__links--img"/>
+                        Users</button>
+                    </div>
+                    <div className="sidebar__bottomVersionText">Version 1.0</div>
+            </form>
          )
     }
 }
 
 export default Sidebar;
+
--- frontend/src/TopBar.js
+++ frontend/src/TopBar.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 
 export default class TopBar extends Component {
+
     render() {
          return (
-            <div></div>
+        <div className="topbar">
+            <form className="topbar__searchBox" id="searchform" onSubmit={this.handleSubmit}>
+                <input
+                    className="topbar__searchBox--input"
+                    type="text"
+                    id="inputSearchBox"
+                    name="Search"
+                    placeholder="Search" 
+                />
+                <img src="./Assets/Icons/Search.svg" className="topbar__searchBox--img"/>
+            </form>
+            <div className="topbar__rightBox">
+                <img src="./Assets/Avatar/Avatar.jpg" className="topbar__rightBox--userImg"/>
+                <div className="topbar__rightBox--optionForm">
+                    <img src="./Assets/Icons/Dropdown.svg" className="topbar__rightBox--optionForm__icon"/>
+                </div>
+            </div>
+        </div>
          )
     }
 }
