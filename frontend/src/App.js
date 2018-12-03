bbc397a0a3cd9682d528761b05f7291ddd2bb528
gizraelian <g.izraelian@gmail.com>
2018-12-02 21:51:09 -0500

    synt test, merge conflict test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,8 +7,6 @@ import Warehouses from './Warehouses';
 import ProductDetail from './ProductDetail';
 import { BrowserRouter as Router, Route, Switch, Redirect,} from 'react-router-dom';
 
-
-
 export default class App extends Component {
   render() {
     return (
